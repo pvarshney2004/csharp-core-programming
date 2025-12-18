71801d5e31c8a2ea8807b5263b256d5d57f372a9
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:04:01 +0530

    Prashant add program to find age by user's birth year

---
+++ Programming_Elements/Level_01/FindAge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_01
+{
+    internal class FindAge
+    {
+        public void CalculateAge(int birthYear)
+        {
+            int currentYear = 2024;
+            int age = currentYear - birthYear;
+            Console.WriteLine("Harry's age in 2024 is: " + age);
+        }
+    }
+}
