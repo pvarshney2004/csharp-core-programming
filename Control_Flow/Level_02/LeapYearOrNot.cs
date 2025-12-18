b11cb9014b6aca7f243f495faa9fc612107c4479
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 16:46:07 +0530

    [Prashant] add program to check whether a year is leap or not

---
+++ Control_Flow/Level_02/LeapYearOrNot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_02
+{
+    internal class LeapYearOrNot
+    {
+        public void IsLeapYear(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine($"{year} is a leap year.");
+            }
+            else
+            {
+                Console.WriteLine($"{year} is not a leap year.");
+            }
+        }
+    }
+}
