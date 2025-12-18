c8ff799e4cd7eca01668c7dfac77b027d53293c7
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:05:27 +0530

    Prashant add program to find total purchase price of items

---
+++ Programming_Elements/Level_01/TotalPurchasePrice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_01
+{
+    internal class TotalPurchasePrice
+    {
+        public void CalculateTotalPurchasePrice(double unitPrice, int quantity)
+        {
+
+            double total = unitPrice * quantity;
+            Console.WriteLine($"The total purchase price is INR {total} if the quantity {quantity} and unit price is INR {unitPrice}");
+        }
+    }
+}
