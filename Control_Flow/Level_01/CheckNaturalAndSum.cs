b68032666652f5bd3dd99b0f51bbcc8e3b9054f1
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 16:48:35 +0530

    [Prashant] add program to check number is natural or not and sum

---
+++ Control_Flow/Level_01/CheckNaturalAndSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class CheckNaturalAndSum
+    {
+        public void Check(int number)
+        {
+            int sum = 0;
+            if (number > 0)
+            {
+                sum = (number * (number + 1)) / 2;
+                Console.WriteLine($"The sum of natural numbers up to {number} is: {sum}");
+            }
+            else
+            {
+                Console.WriteLine("The number is not a natural number.");
+            }
+        }
+    }
+}
