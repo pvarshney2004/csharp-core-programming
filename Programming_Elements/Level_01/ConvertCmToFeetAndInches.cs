117cda8f00c1dbcb04c045163f5c5334c27114a1
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 14:48:40 +0530

    Prashant add program to convert cm to ft and inches

---
+++ Programming_Elements/Level_01/ConvertCmToFeetAndInches.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_01
+{
+    internal class ConvertCmToFeetAndInches
+    {
+        public void ConvertCm(double cm)
+        {
+            double totalInches = cm / 2.54;
+            int feet = (int)(totalInches / 12);
+            double inches = totalInches % 12;
+            Console.WriteLine($"{cm} cm is equal to {feet} feet and {inches:F2} inches.");
+        }
+    }
+}
