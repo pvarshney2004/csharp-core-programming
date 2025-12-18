9736798615957780c0bb0c8f3efa0c47820e9021
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 14:49:01 +0530

    Prashant add program to convert distance from km to miles

---
+++ Programming_Elements/Level_01/ConvertDistance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_01
+{
+    internal class ConvertDistance
+    {
+        public void ConvertKmToMiles(double kilometers)
+        {
+            double miles = kilometers * 0.621;
+            Console.WriteLine(kilometers + " kilometers is equal to " + miles + " miles.");
+        }
+    }
+}
