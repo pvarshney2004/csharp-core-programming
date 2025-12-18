51bcf5d7896a9a2fb5b472a14d77a467d532be9b
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:09:08 +0530

    Prashant add program to calculate simple interest

---
+++ Programming_Elements/Level_02/CalculateSimpleInterest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_02
+{
+    internal class CalculateSimpleInterest
+    {
+        public void ComputeSimpleInterest(double principal, double rate, double time)
+        {
+            double simpleInterest = (principal * rate * time) / 100;
+            Console.WriteLine($"The Simple Interest is: {simpleInterest}");
+        }
+    }
+}
