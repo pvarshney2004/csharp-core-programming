be03c9b6ae2c93061e2b7f1335e9d679019fd500
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 17:07:20 +0530

    [Prashant] add Program.cs file

---
+++ Control_Flow/Level_01/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            Creation of object and method call
+            ----------------------------------
+            */
+
+            //CheckDivisibility obj1 = new CheckDivisibility();
+            //obj1.Check(10);
+
+            //IsFirstSmallest obj2 = new IsFirstSmallest();
+            //obj2.Check(5, 10, 15);
+            //obj2.Check(20, 10, 15);
+
+            //CheckNaturalAndSum obj3 = new CheckNaturalAndSum();
+            //obj3.Check(5);
+            //obj3.Check(-3);
+
+            //CanVote obj4 = new CanVote();
+            //obj4.Check(20);
+            //obj4.Check(15);
+
+            //MultiplicationTable obj5 = new MultiplicationTable();
+            //obj5.PrintMultiplicationTableFrom6To9(7);
+
+            //FindBonus obj6 = new FindBonus();
+            //obj6.CalculateBonus(6, 50000);
+
+            //SumNaturalNumbers obj7 = new SumNaturalNumbers();
+            //obj7.SumNumbers(5);
+
+            //CountDownToOne obj9 = new CountDownToOne();
+            //obj9.CountDown(5);
+
+            //CountDownToOne02 obj10 = new CountDownToOne02();
+            //obj10.CountDown(5);
+
+            //SpringSeason obj11 = new SpringSeason();
+            //obj11.CheckSpringSeason(25, 4);
+            //obj11.CheckSpringSeason(15, 7);
+
+            //CheckPositiveNegativeZero obj12 = new CheckPositiveNegativeZero();
+            //obj12.Check(10);
+            //obj12.Check(-5);
+
+            //PrintOddEven obj13 = new PrintOddEven();
+            //obj13.PrintOddEvenNumbers(10);
+
+            //FactorialUsingFor obj14 = new FactorialUsingFor();
+            //obj14.CalculateFactorial(5);
+
+            //FactorialUsingWhile obj15 = new FactorialUsingWhile();
+            //obj15.CalculateFactorial(5);
+
+            //SumNaturalNumbersFor obj16 = new SumNaturalNumbersFor();
+            //obj16.SumUsingFor(5);
+
+            SumUntilUserEnterZero obj8 = new SumUntilUserEnterZero();
+            obj8.CalculateSum();
+
+
+
+
+
+
+
+
+        }
+    }
+}
