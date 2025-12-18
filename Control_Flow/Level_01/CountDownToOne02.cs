1bab83dffe69cc0b28fbf1d4dd4ab14c3d609e6a
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 17:04:13 +0530

    [Prashant] add program to count down to one using for loop

---
+++ Control_Flow/Level_01/CountDownToOne02.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class CountDownToOne02
+    {
+        public void CountDown(int number)
+        {
+            for (; number >= 1; number--)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
