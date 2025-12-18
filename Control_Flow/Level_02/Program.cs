7bd22bdfd3935a16dbd0d9c92d3c3490928f45bc
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 17:07:30 +0530

    [Prashant] add Program.cs file

---
+++ Control_Flow/Level_02/Program.cs
@@ -0,0 +1,15 @@
+﻿using Core_Programming.Control_Flow.Level_02;
+using System;
+namespace Core_Programming.Programming_Elements.Level_02 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LeapYearOrNot obj1 = new LeapYearOrNot();
+            Console.WriteLine("Enter a year to check if it's a leap year or not:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            obj1.IsLeapYear(year);
+        }
+    }
+}
