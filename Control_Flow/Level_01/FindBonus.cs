41594498c1cee1a13d6f1760b8714eac470a4c24
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 17:06:47 +0530

    [Prashant] add program to find bonus using bonus percent

---
+++ Control_Flow/Level_01/FindBonus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class FindBonus
+    {
+        public void CalculateBonus(int yearsOfService, double salary)
+        {
+            double bonus = 0;
+            if (yearsOfService > 5)
+            {
+                bonus = salary * 0.05;
+            }
+            else
+            {
+                bonus = 0;
+            }
+            Console.WriteLine($"Employee with {yearsOfService} years of service and salary {salary} receives a bonus of {bonus}");
+        }
+    }
+}
