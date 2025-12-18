d117c54cc1a2b595bb4b31e67cb4a80c36544b06
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 17:05:42 +0530

    [Prashant] add program to find Is the first number out of three is smallest or not

---
+++ Control_Flow/Level_01/IsFirstSmallest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class IsFirstSmallest
+    {
+        public void Check(int a, int b, int c)
+        {
+            if (a < b && a < c)
+            {
+                Console.WriteLine($"{a} is the smallest number.");
+            }
+            else
+            {
+                Console.WriteLine($"{a} is not the smallest number.");
+            }
+        }
+    }
+}
