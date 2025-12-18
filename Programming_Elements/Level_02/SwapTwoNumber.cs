6b782aebc9aae5d07d32943e9e75cb5f1891d47f
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:11:31 +0530

    Prashant add program to swap two numbers

---
+++ Programming_Elements/Level_02/SwapTwoNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_02
+{
+    internal class SwapTwoNumber
+    {
+        public void SwapNumbers(int a, int b)
+        {
+            Console.WriteLine("Before Swapping: ");
+            Console.WriteLine("First Number: " + a);
+            Console.WriteLine("Second Number: " + b);
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After Swapping: ");
+            Console.WriteLine("First Number: " + a);
+            Console.WriteLine("Second Number: " + b);
+        }
+    }
+}
