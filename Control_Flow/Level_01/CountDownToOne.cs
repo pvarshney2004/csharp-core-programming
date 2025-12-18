64b0ea988c70b240dc7613e7579a0ebe8f7b21d7
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 17:03:53 +0530

    [Prashant] add program to count down to one using while loop

---
+++ Control_Flow/Level_01/CountDownToOne.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class CountDownToOne
+    {
+        public void CountDown(int number) 
+        {
+            while(number>=1) 
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+        }
+    }
+}
