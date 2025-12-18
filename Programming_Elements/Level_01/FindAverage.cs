cd37fadbe7362dd8504f442abeb33b5bac2dcddf
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:04:17 +0530

    Prashant add program to find average of marks

---
+++ Programming_Elements/Level_01/FindAverage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_01
+{
+    internal class FindAverage
+    {
+        public void CalculateAverage(int marks1, int marks2, int marks3)
+        {
+            double average = (marks1 + marks2 + marks3) / 3.0;
+            Console.WriteLine("Sam’s average mark in PCM is " + average);
+        }
+    }
+}
