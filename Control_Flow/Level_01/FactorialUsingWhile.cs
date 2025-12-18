874d5bc57159657484fc889913d0af3cb848104a
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 17:04:42 +0530

    [Prashant] add program to find factorial using while loop

---
+++ Control_Flow/Level_01/FactorialUsingWhile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class FactorialUsingWhile
+    {
+        public void CalculateFactorial(int n)
+        {
+            int factorial = 1;
+            int i = 1;
+            while (i <= n)
+            {
+                factorial *= i;
+                i++;
+            }
+            Console.WriteLine($"Factorial of {n} is {factorial}");
+        }
+    }
+}
