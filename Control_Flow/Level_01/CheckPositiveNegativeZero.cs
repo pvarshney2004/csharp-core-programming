ff90ab8b4528123eb1b4eee0e199226eeed8dadf
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 16:50:52 +0530

    [Prashant] add program to check number is positive, negative and zero

---
+++ Control_Flow/Level_01/CheckPositiveNegativeZero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class CheckPositiveNegativeZero
+    {
+        public void Check(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"{number} is Positive");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"{number} is Negative");
+            }
+            else
+            {
+                Console.WriteLine("The number is Zero");
+            }
+        }
+    }
+}
