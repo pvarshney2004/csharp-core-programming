8e39be9ff9743a26859c118477fbce2eea9d7ed6
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 14:50:06 +0530

    Prashant add program to calculate course discount fee

---
+++ Programming_Elements/Level_01/CourseDiscount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_01
+{
+    internal class CourseDiscount
+    {
+        public void FindDiscount()
+        {
+            double fee = 125000;
+            double discountPercent = 10;
+            double discount = fee * discountPercent / 100;
+            double finalFee = fee - discount;
+            Console.WriteLine($"The discount amount is INR {discount} and final discounted fee is INR {finalFee}");
+        }
+    }
+}
