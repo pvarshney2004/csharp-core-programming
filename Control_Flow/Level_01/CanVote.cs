21eccf6a75427e97b13e3d148350f690c71f608a
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 16:03:18 +0530

    [Prashant] add program to check if a person can vote or not

---
+++ Control_Flow/Level_01/CanVote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class CanVote
+    {
+        public void Check(int age)
+        {
+            if (age >= 18)
+            {
+                Console.WriteLine("You are eligible to vote.");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible to vote.");
+            }
+        }
+    }
+}
