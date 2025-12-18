651e0c7004dc9d61040f35b3f6d6a78cf1dc7439
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:09:25 +0530

    Prashant add program to convert weight

---
+++ Programming_Elements/Level_02/ConvertWeight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_02
+{
+    internal class ConvertWeight
+    {
+        public void ConvertPoundsToKg(double pounds)
+        {
+            double kg = pounds * 0.45359237;
+            Console.WriteLine($"{pounds} pounds is equal to {kg:F4} kilograms.");
+        }
+    }
+}
