8aa952545307e3eb38a38cbc1303ac123cb64a2d
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:05:04 +0530

    Prashant add program to find side of square by its perimeter

---
+++ Programming_Elements/Level_01/FindSquareSide.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_01
+{
+    internal class FindSquareSide
+    {
+        public void calculateSide(double perimeter)
+        {
+            double side = perimeter / 4;
+            Console.WriteLine("The length of one side of the square is: " + side);
+
+        }
+    }
+}
