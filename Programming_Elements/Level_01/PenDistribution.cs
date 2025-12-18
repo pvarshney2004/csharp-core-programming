4eeb6733a7849ff02e8cdd1887bfc077c085b01b
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:06:30 +0530

    Prashant add program to calculate pen distribution per person

---
+++ Programming_Elements/Level_01/PenDistribution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_01
+{
+    internal class PenDistribution
+    {
+        public void DistributePens(int totalPens, int numberOfStudents)
+        {
+            int pensPerStudent = totalPens / numberOfStudents;
+            int remainingPens = totalPens % numberOfStudents;
+            Console.WriteLine("Each student will get " + pensPerStudent + " pens.");
+            Console.WriteLine("Number of pens remaining: " + remainingPens);
+        }
+    }
+}
