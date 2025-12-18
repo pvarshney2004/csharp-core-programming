c1b80adf65a68c53fe4e89f935b75ec32b9b5d2f
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 16:50:05 +0530

    [Prashant] add program to sum natural number using for loop

---
+++ Control_Flow/Level_01/SumNaturalNumbersFor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class SumNaturalNumbersFor
+    {
+        public void SumUsingFor(int n) {
+            int sumUsingForLoop = 0;
+            int sumUsingFormula = 0;
+            for(int i=1; i<=n; i++) 
+            {
+                sumUsingForLoop += i;
+            }
+            sumUsingFormula = n * (n + 1) / 2;
+            Console.WriteLine($"Sum of first {n} natural numbers using for loop: {sumUsingForLoop}");
+            Console.WriteLine($"Sum of first {n} natural numbers using formula: {sumUsingFormula}");
+        }
+    }
+}
