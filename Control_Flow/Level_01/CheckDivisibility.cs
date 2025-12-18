78f85a774670b367cfbde8e5b434963832ad6a41
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:45:38 +0530

    Prashant add program to check a number is divisible by 5 or not

---
+++ Control_Flow/Level_01/CheckDivisibility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class CheckDivisibility
+    {
+        public void check(int num)
+        {
+           bool divisibleBy5 = (num % 5 == 0);
+            if (divisibleBy5)
+            {
+                Console.WriteLine($"{num} is divisible by 5.");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not divisible by 5.");
+            }
+        }
+    }
+}
