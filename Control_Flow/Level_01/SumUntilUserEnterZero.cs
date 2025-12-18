c12380ac9883f120bf293709caa204d4a7dfa6ad
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 16:49:17 +0530

    [Prashant] add program to sum numbers until user enter zero

---
+++ Control_Flow/Level_01/SumUntilUserEnterZero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class SumUntilUserEnterZero
+    {
+        public void CalculateSum()
+        {
+            int sum = 0;
+            Console.WriteLine("Enter numbers to sum them up. Enter 0 to stop.");
+            while (true)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                if (num == 0)
+                {
+                    break;
+                }
+                sum+= num;
+            }
+            Console.WriteLine($"The total sum is: {sum}");
+        }
+    }
+}
