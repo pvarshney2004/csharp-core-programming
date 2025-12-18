d4ac23634ee18aa56aae23069cd9bdc4bda3eed4
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 14:42:58 +0530

    Prashant add program to perform basic calculations

---
+++ Programming_Elements/Level_01/BasicCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_01
+{
+    internal class BasicCalculator
+    {
+        public void PerformCalculation(double num1, double num2)
+        {
+            double addition = num1 + num2;
+            double subtract = num1 - num2;
+            double multiplication = num1 * num2;
+            double division = num1 / num2;
+            Console.WriteLine($"The addition, subtraction, multiplication and division value of 2 numbers {num1} and {num2} is {addition}, {subtract}, {multiplication}, and {division}.");
+        }
+    }
+}
