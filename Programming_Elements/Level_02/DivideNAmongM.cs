9b03a8964f001bdd5a200cab6faddea9ba37a23d
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:09:48 +0530

    Prashant add program to divide N chochlates among M children

---
+++ Programming_Elements/Level_02/DivideNAmongM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_02
+{
+    internal class DivideNAmongM
+    {
+        public void Distribute(int n, int m)
+        {
+            int quotient = n / m;
+            int remainder = n % m;
+            Console.WriteLine($"The number of chocolates each child gets is {quotient} and the number of remaining chocolates is {remainder}");
+        }
+    }
+}
