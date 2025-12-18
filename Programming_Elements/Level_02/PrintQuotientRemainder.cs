319e2af3cef7226cd871cd446d361ae8428c9972
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:10:04 +0530

    Prashant add program to print quotient and remainder

---
+++ Programming_Elements/Level_02/PrintQuotientRemainder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_02
+{
+    internal class PrintQuotientRemainder
+    {
+        public void CalculateQuotientRemainder(int num1, int num2)
+        {
+            int quotient = num1 / num2;
+            int remainder = num1 % num2;
+            Console.WriteLine($"Quotient: {quotient} and Remainder: {remainder}");
+        }
+    }
+}
