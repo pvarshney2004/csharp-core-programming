c026d65bf0d52bed1eabde7ebd7ca64b056c89c1
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 17:06:02 +0530

    [Prashant] add program to print multiplication table

---
+++ Control_Flow/Level_01/MultiplicationTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class MultiplicationTable
+    {
+        public void PrintMultiplicationTableFrom6To9(int num)
+        {
+            for (int i = 6; i <= 9; i++)
+            {
+                Console.WriteLine($"Multiplication Table of {i}:");
+                for (int j = 1; j <= num; j++)
+                {
+                    Console.WriteLine($"{i} x {j} = {i * j}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
