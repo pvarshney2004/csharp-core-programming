cdb229a7a81ce2e1397edd81a174d9d3bbfb40b5
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:10:30 +0530

    Prashant add program to convert temperature from cel to farh

---
+++ Programming_Elements/Level_02/TemperatureConversion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Core_Programming.Programming_Elements.Level_02
+{
+    internal class TemperatureConversion
+    {
+        public void convertCelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine($"{celsius}°C is equal to {fahrenheit}°F");
+        }
+    }
+}
