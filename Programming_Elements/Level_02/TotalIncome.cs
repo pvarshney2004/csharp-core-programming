45df92d76d19a29a2e1ea6e15933121447bafb65
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:11:48 +0530

    Prashant add program to calculate total income by income and bonus

---
+++ Programming_Elements/Level_02/TotalIncome.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_02
+{
+    internal class TotalIncome
+    {
+        public void CalculateTotalIncome(int income, int bonus)
+        {
+            Console.WriteLine("Total Income: " + (income + bonus) + " INR");
+        }
+    }
+}
