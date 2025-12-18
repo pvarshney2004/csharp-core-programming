4e5ba70fa2e346a3befef08906894fed0d4a4d32
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:08:52 +0530

    Prashant add program to calculate round of an athlete

---
+++ Programming_Elements/Level_02/CalculateRounds.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_02
+{
+    internal class CalculateRounds
+    {
+        public void calculate(int side1, int side2, int side3) { 
+            int perimeterInMeter = side1 + side2 + side3;
+            int distanceToRunInKM = 5;
+            int perimeterInKM = perimeterInMeter / 1000;
+            int rounds = distanceToRunInKM / perimeterInKM;
+            Console.WriteLine("Number of rounds the athlete has to run: " + rounds);
+
+        }
+    }
+}
