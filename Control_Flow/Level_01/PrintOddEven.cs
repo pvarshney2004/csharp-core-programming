e599b28b2c7b8423fc80818b86cb90af70d13729
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 17:06:27 +0530

    [Prashant] add program to print odd and even between 1 to number

---
+++ Control_Flow/Level_01/PrintOddEven.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class PrintOddEven
+    {
+        public void PrintOddEvenNumbers(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"{i} is Even");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} is Odd");
+                }
+            }
+        }
+    }
+}
