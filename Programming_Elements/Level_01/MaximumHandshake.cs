1175e9c61716730626fc7fb13f364ba3450270cf
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 15:06:14 +0530

    Prashant add program to calculate maximum handshakes

---
+++ Programming_Elements/Level_01/MaximumHandshake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_01
+{
+    internal class MaximumHandshake
+    {
+        public void calculateMaxHandshakes(int numberOfStudents)
+        {
+            int maxHandshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+            Console.WriteLine("Maximum number of handshakes among " + numberOfStudents + " people is: " + maxHandshakes);
+        }
+    }
+}
