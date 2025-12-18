c23f1e5b681764a4bdff028b7d1295e4c149b4b0
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 17:04:32 +0530

    [Prashant] add program to find factorial using for loop

---
+++ Control_Flow/Level_01/FactorialUsingFor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_01
+{
+    internal class FactorialUsingFor
+    {
+        public void CalculateFactorial(int n)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine($"Factorial of {n} is {factorial}");
+        }
+    }
+}
