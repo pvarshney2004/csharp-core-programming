23a412eb5589f1fd572f52d4d6a873a230aa7e27
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-18 14:49:32 +0530

    Prashant add program to convert distance from ft to yards and miles

---
+++ Programming_Elements/Level_01/ConvertFeetToYardsAndMiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Programming_Elements.Level_01
+{
+    internal class ConvertFeetToYardsAndMiles
+    {
+        public void convert(double distanceInFeet)
+        {
+            double yards = distanceInFeet / 3.0;
+            double miles = distanceInFeet / 5280.0;
+            Console.WriteLine($"{distanceInFeet} feet is equal to {yards} yards and {miles} miles.");
+        }
+    }
+}
