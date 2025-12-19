a2c82ca290c9bd0216a1cd06113644540ccbb15c
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 16:33:53 +0530

    [Prashant] add program to find height of players using array

---
+++ Arrays/Level_01/MeanHeight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Arrays.Level_01
+{
+    internal class MeanHeight
+    {
+        public void Calculate(double[] height)
+        {
+            double sum = 0;
+            for (int i = 0; i < height.Length; i++)
+            {
+                sum += height[i];
+            }
+            double mean = sum / height.Length;
+            Console.WriteLine("Mean Height: " + mean);
+        }
+    }
+}
