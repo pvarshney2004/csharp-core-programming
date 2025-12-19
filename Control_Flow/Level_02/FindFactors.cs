9c5a7ab26424f91a05f78b53d00a6a1d4c6f2a7b
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:33:36 +0530

    [Prashant] add program to find factors of a number

---
+++ Control_Flow/Level_02/FindFactors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_02
+{
+    internal class FindFactors
+    {
+        public void DisplayFactors(int number)
+        {
+            if (number <= 0)
+            {
+                Console.WriteLine("Number must be greater than zero.");
+                return;
+            }
+            Console.WriteLine($"Factors of {number} are:");
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
