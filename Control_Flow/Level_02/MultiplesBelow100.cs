a0fe3543c19acfb4ff83fbd6d541011c8aae73f8
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:32:07 +0530

    [Prashant] add program to find mulpiples of a number below 100

---
+++ Control_Flow/Level_02/MultiplesBelow100.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_02
+{
+    internal class MultiplesBelow100
+    {
+        public void DisplayMultiples(int number)
+        {
+           
+            Console.WriteLine($"Multiples of {number} below 100 are:");
+            for (int i = 100; i > 1; i--)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
