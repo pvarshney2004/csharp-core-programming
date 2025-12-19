e41111fd33c1277bafe96468d895cc654c95732d
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 16:33:34 +0530

    [Prashant] add program to print multiplication table of a number using array

---
+++ Arrays/Level_01/MultiplicationTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Arrays.Level_01
+{
+    internal class MultiplicationTable
+    {
+        public void PrintTable(int number)
+        {
+            int[] result = new int[10];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = number * (i + 1);
+                Console.WriteLine(number + " x " + (i + 1) + " = " + result[i]);
+            }
+        }
+    }
+}
