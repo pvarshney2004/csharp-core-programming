d1ae2b121b560145cea0e8e1f4081f162bf05e98
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 16:29:13 +0530

    [Prashant] add program to check number is positive,negative,zero using arrays

---
+++ Arrays/Level_01/CheckPositiveNegativeZero.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Arrays.Level_01
+{
+    internal class CheckPositiveNegativeZero
+    {
+        public void Check(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    Console.WriteLine(nums[i] + " is Positive");
+                }
+                else if (nums[i] < 0)
+                {
+                    Console.WriteLine(nums[i] + " is Negative");
+                }
+                else
+                {
+                    Console.WriteLine(nums[i] + " is Zero");
+                }
+            }
+            if (nums[0] == nums[nums.Length - 1])
+            {
+                Console.WriteLine("First and Last elements are equal: " + nums[0]);
+            }
+            else
+            {
+                Console.WriteLine("First and Last elements are not equal: " + nums[0] + " and " + nums[nums.Length - 1]);
+            }
+        }
+    }
+}
