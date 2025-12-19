57310fdd47ad4bc2e2636de0112a4f14100f43d1
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:33:57 +0530

    [Prashant] add program to find number raised to power

---
+++ Control_Flow/Level_02/FindPower.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_02
+{
+    internal class FindPower
+    {
+        public void CalculatePower(int number, int power)
+        {
+            int result = 1;
+            for(int i=1; i<=power; i++)
+            {
+                result *= number;
+            }
+            Console.WriteLine($"{number} raised to the power of {power} is: {result}");
+
+        }
+    }
+}
