cd506b8cf1d35e8748c1271ff7a53a09fac9f2da
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 16:49:20 +0530

    [Prashant] add program to find factors of a number

---
+++ Arrays/Level_01/FindFactors.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Arrays.Level_01
+{
+    internal class FindFactors
+    {
+        public void Find(int number)
+        {
+            int[] factors = new int[10];
+            int index = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    if (index < factors.Length)
+                    {
+                        factors[index] = i;
+                        index++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Factor array is full. Cannot store more factors.");
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine($"Factors of {number} are:");
+            for (int i = 0; i < index; i++)
+            {
+                Console.WriteLine(factors[i]);
+            }
+        }
+    }
+}
