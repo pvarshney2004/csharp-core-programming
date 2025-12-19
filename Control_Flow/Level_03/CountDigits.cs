2497cb31d1da3d8fa495d2f128aec1b834557f9f
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:28:36 +0530

    [Prashant] add program to count number of digits in number

---
+++ Control_Flow/Level_03/CountDigits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_03
+{
+    internal class CountDigits
+    {
+        public void CountNumberOfDigits(int number)
+        {
+            int count = 0;
+            while (number != 0)
+            {
+                number = number / 10;
+                count++;
+            }
+            Console.WriteLine("Number of digits: " + count);
+        }
+    }
+}
