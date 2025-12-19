95a6367ab02952bbd0dc0bee83b77fe3d3d23668
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:31:47 +0530

    [Prashant] add program to find BMI of person

---
+++ Control_Flow/Level_02/FindBMI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_02
+{
+    internal class FindBMI
+    {
+        public void CalculateBMI(double weightInKg, double heightInMeters)
+        {
+            if (heightInMeters <= 0)
+            {
+                Console.WriteLine("Height must be greater than zero.");
+                return;
+            }
+            double bmi = weightInKg / (heightInMeters * heightInMeters);
+            Console.WriteLine($"Your BMI is: {bmi:F2}");
+            if (bmi < 18.5)
+            {
+                Console.WriteLine("You are underweight.");
+            }
+            else if (bmi >= 18.5 && bmi < 24.9)
+            {
+                Console.WriteLine("You have a normal weight.");
+            }
+            else if (bmi >= 25 && bmi < 29.9)
+            {
+                Console.WriteLine("You are overweight.");
+            }
+            else
+            {
+                Console.WriteLine("You are obese.");
+            }
+        }
+    }
+}
