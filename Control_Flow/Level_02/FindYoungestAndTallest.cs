6eb4246f3b354ce2b3b4393592f5392afc17b4a8
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:33:21 +0530

    [Prashant] add program to fing youngest and tallest

---
+++ Control_Flow/Level_02/FindYoungestAndTallest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_02
+{
+    internal class FindYoungestAndTallest
+    {
+        public void FindYoungest(int age1, int age2, int age3)
+        {
+            int youngestAge = age1;
+            if (age2 < youngestAge)
+            {
+                youngestAge = age2;
+            }
+            if (age3 < youngestAge)
+            {
+                youngestAge = age3;
+            }
+            Console.WriteLine($"The youngest age is: {youngestAge}");
+        }
+
+        public void FindTallest(double height1, double height2, double height3)
+        {
+            double tallestHeight = height1;
+            if (height2 > tallestHeight)
+            {
+                tallestHeight = height2;
+            }
+            if (height3 > tallestHeight)
+            {
+                tallestHeight = height3;
+            }
+            Console.WriteLine($"The tallest height is: {tallestHeight} meters");
+        }
+    }
+}
