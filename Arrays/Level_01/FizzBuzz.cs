a2057f96828bded2fb2579f34e938ca2fe4f4eba
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 16:33:06 +0530

    [Prashant] add program to print FizzBuzz of a number using array

---
+++ Arrays/Level_01/FizzBuzz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Arrays.Level_01
+{
+    internal class FizzBuzz
+    {
+        public void PrintFizzBuzz(int n)
+        {
+            String[] results = new String[n];
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    results[i - 1] = "FizzBuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    results[i - 1] = "Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    results[i - 1] = "Buzz";
+                }
+                else
+                {
+                    results[i - 1] = i.ToString();
+                }
+            }
+            for(int i = 0; i < results.Length; i++)
+            {
+                Console.WriteLine($"Position {i+1}: {results[i]}");
+            }
+        }
+    }
+}
