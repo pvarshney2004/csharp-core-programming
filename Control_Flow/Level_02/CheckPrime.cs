ad4f3f598bbb3cb07f400f8ed8b46f72a7eb268d
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:31:29 +0530

    [Prashant] add program to check if a number is prime or not

---
+++ Control_Flow/Level_02/CheckPrime.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_02
+{
+    internal class CheckPrime
+    {
+        public void IsPrime(int number)
+        {
+            bool isPrime = true;
+            if (number <= 1)
+            {
+                isPrime = false;
+            }
+            else
+            {
+                for (int i = 2; i <= Math.Sqrt(number); i++)
+                {
+                    if (number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine($"{number} is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a prime number.");
+            }
+        }
+    }
+}
