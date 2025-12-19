9848d1510ede6d13c9c688f02ef9efe673578829
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:29:20 +0530

    [Prashant] add program to perform basic calculations using switch

---
+++ Control_Flow/Level_03/CalculatorUsingSwitch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_03
+{
+    internal class CalculatorUsingSwitch
+    {
+        public void Calculate(double num1, double num2, string op)
+        {
+            double result;
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine($"{num1} + {num2} = {result}");
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine($"{num1} - {num2} = {result}");
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine($"{num1} * {num2} = {result}");
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine($"{num1} / {num2} = {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Division by zero is not allowed.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator. Please use +, -, *, or /.");
+                    break;
+            }
+        }
+    }
+}
