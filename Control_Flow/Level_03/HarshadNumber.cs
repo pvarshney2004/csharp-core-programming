c48e862af984369f876cfd095e1cb49ad7f3b952
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:27:57 +0530

    [Prashant] add program to check if a number is Harshad Number

---
+++ Control_Flow/Level_03/HarshadNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_03
+{
+    internal class HarshadNumber
+    {
+        public void CheckHarshad(int number)
+        {
+            int temp = number;
+            int sum = 0;
+            while (temp > 0)
+            {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            if (number % sum == 0)
+            {
+                Console.WriteLine(number + " is a Harshad number.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is not a Harshad number.");
+            }
+        }
+    }
+}
