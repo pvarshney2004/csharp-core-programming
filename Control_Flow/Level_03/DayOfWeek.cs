7bb70e1c85827dfe08b2c7e67f42f1bd5b55cf38
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:28:20 +0530

    [Prashant] add program to find day of week using day/month/year

---
+++ Control_Flow/Level_03/DayOfWeek.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_03
+{
+    internal class DayOfWeek
+    {
+        public void GetDayName(int day, int month, int year)
+        {
+            int y0 = year - (14 - month) / 12;
+            int x = y0 + (y0 / 4) - (y0 / 100) + (y0 / 400);
+            int m0 = month + 12 * ((14 - month) / 12) - 2;
+            int d0 = (day + x + (31 * m0) / 12) % 7;
+            switch (d0)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Date");
+                    break;
+
+            }
+        }
+    }
+}
