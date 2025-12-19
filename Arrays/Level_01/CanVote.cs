cda5a97fdd17287053d8f33f7e2146f6c9559f08
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 16:28:19 +0530

    [Prashant] add program to check if a person with given age can vote or not using arrays

---
+++ Arrays/Level_01/CanVote.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Arrays.Level_01
+{
+    internal class CanVote
+    {
+        public void Find(int[] ages)
+        {
+            for (int i = 0; i < ages.Length; i++)
+            {
+                if (ages[i] >= 18)
+                {
+                    Console.WriteLine("Age " + ages[i] + ": Eligible to vote");
+                }
+                else
+                {
+                    Console.WriteLine("Age " + ages[i] + ": Not eligible to vote");
+                }
+            }
+        }
+    }
+}
