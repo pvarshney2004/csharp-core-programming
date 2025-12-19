3001c9e8ecc2b284ef595802aeeb0ffcf9940737
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:27:38 +0530

    [Prashant] add program to check if a number is armstrong or not

---
+++ Control_Flow/Level_03/IsArmstrong.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_03
+{
+    internal class IsArmstrong
+    {
+        public void CheckArmstrong(int number)
+        {
+            int originalNumber = number;
+            int sum = 0;
+            int len = number.ToString().Length;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                sum += (int)Math.Pow(digit, len);
+                number /= 10;
+            }
+            if (sum == originalNumber)
+            {
+                Console.WriteLine($"{originalNumber} is an Armstrong number.");
+            }
+            else
+            {
+                Console.WriteLine($"{originalNumber} is not an Armstrong number.");
+            }
+        }
+    }
+}
