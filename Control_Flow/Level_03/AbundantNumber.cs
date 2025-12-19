2f07de1a083499bbf9d30bf11c1f8ac224879b98
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:27:01 +0530

    [Prashant] add program to find abundant number

---
+++ Control_Flow/Level_03/AbundantNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_03
+{
+    internal class AbundantNumber
+    {
+        public void CheckAbundant(int number)
+        {
+            int sumOfDivisors = 0;
+            for (int i = 1; i <= number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    sumOfDivisors += i;
+                }
+            }
+            if (sumOfDivisors > number)
+            {
+                Console.WriteLine($"{number} is an Abundant number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not an Abundant number.");
+            }
+        }
+    }
+}
