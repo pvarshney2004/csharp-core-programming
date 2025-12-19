b17001d7f72709d698f0d347d9297c4f843385d8
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-19 11:32:59 +0530

    [Prashant] add program to print FizzBuzz for a number

---
+++ Control_Flow/Level_02/FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Control_Flow.Level_02
+{
+    internal class FizzBuzz
+    {
+        public void PrintFizzBuzz(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
