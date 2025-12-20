4a1e82dd38dabf56c13e72fc67e96bc3a5f98c5c
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-20 11:57:09 +0530

    [Prashant] add Program.cs file

---
+++ Arrays/Level_02/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+namespace Core_Programming.Arrays.Level_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //FindFrequency obj1 = new FindFrequency();
+            //obj1.Solve(num);
+
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //ReverseNumber obj2 = new ReverseNumber();
+            //obj2.Solve(num);
+
+            //FindYoungestAndTallest obj3 = new FindYoungestAndTallest();
+            //int[] ages = new int[3];
+            //Console.Write("Enter ages of 3 persons: ");
+            //for(int i=0; i<3; i++)
+            //{
+            //    ages[i]=Convert.ToInt32(Console.ReadLine());
+            //}
+            //obj3.FindYoungest(ages);
+            //double[] heights = new double[3];
+            //Console.Write("Enter heights of 3 persons: ");
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    heights[i] = Convert.ToDouble(Console.ReadLine());
+            //}
+            //obj3.FindTallest(heights);
+
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //FindLargest obj4 = new FindLargest();
+            //obj4.FindLargestAndSecondLargest(number);
+
+            //double[] salary = new double[10];
+            //Console.WriteLine("Enter the salary of 10 employees: ");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    salary[i] = Convert.ToDouble(Console.ReadLine());
+            //}
+            //double[] yearsOfService = new double[10];
+            //Console.WriteLine("Enter the years of service of 10 employees: ");
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    yearsOfService[i] = Convert.ToDouble(Console.ReadLine());
+            //}
+            //FindBonus obj5 = new FindBonus();
+            //obj5.Solve(salary, yearsOfService);
+
+            //int numberOfPersons = Convert.ToInt32(Console.ReadLine());
+            //double[] weightInKG = new double[numberOfPersons];
+            //double[] heightInMeters = new double[numberOfPersons];
+            //for (int i = 0; i < numberOfPersons; i++)
+            //{
+            //    Console.WriteLine($"Enter weight (in kg) of person {i + 1}: ");
+            //    weightInKG[i] = Convert.ToDouble(Console.ReadLine());
+            //    Console.WriteLine($"Enter height (in meters) of person {i + 1}: ");
+            //    heightInMeters[i] = Convert.ToDouble(Console.ReadLine());
+            //}
+            //FindBMI obj6 = new FindBMI();
+            //obj6.CalculateBMI(weightInKG, heightInMeters);
+
+            //int numberOfStudents = Convert.ToInt32(Console.ReadLine());
+            //int[] physics = new int[numberOfStudents];
+            //int[] chemistry = new int[numberOfStudents];
+            //int[] mathematics = new int[numberOfStudents];
+            //for (int i = 0; i < numberOfStudents; i++)
+            //{
+            //    Console.WriteLine($"Enter marks of physics, chemistry and maths of student {i + 1}: ");
+            //    physics[i] = Convert.ToInt32(Console.ReadLine());
+            //    chemistry[i] = Convert.ToInt32(Console.ReadLine());
+            //    mathematics[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //FindPercentAndGrade obj7 = new FindPercentAndGrade();
+            //obj7.CalculatePercentAndGrade(physics,chemistry,mathematics);
+
+            //int numberOfStudents = Convert.ToInt32(Console.ReadLine());
+            //// this 2-D array will hold marks of each student in 3 subjects: physics, chemistry, mathematics
+            //int[,] marks = new int[numberOfStudents, 3];
+            //for (int i = 0; i < numberOfStudents; i++)
+            //{
+            //    Console.WriteLine($"Enter marks of physics, chemistry and maths of student {i + 1}: ");
+            //    marks[i, 0] = Convert.ToInt32(Console.ReadLine()); // physics
+            //    marks[i, 1] = Convert.ToInt32(Console.ReadLine()); // chemistry
+            //    marks[i, 2] = Convert.ToInt32(Console.ReadLine()); // mathematics
+            //}
+            //FindPercentAndGrade02 obj8 = new FindPercentAndGrade02();
+            //obj8.FindUsing2DArray(marks);
+
+        }
+    }
+}
