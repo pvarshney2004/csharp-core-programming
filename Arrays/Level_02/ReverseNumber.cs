2529e9d779fda921688793d460a0c7fd7471c299
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-20 11:56:17 +0530

    [Prashant] add program to find reverse of number using array

---
+++ Arrays/Level_02/ReverseNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Arrays.Level_02
+{
+    internal class ReverseNumber
+    {
+        public void Solve(int num)
+        {
+            int count = Count(num);
+            int[] digits = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                digits[i] = num % 10;
+                num = num / 10;
+            }
+            Console.WriteLine("Reversed Number: " + string.Join("", digits));
+        }
+        public int Count(int num)
+        {
+            int count = 0;
+            while (num > 0)
+            {
+                count++;
+                num = num / 10;
+            }
+            return count;
+        }
+    }
+}
