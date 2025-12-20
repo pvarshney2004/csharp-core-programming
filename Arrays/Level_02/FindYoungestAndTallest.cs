b4c99789cd32e57160bfba2a725f11b10ea9a8ba
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-20 11:55:51 +0530

    [Prashant] add program to find youngest and tallest person using array

---
+++ Arrays/Level_02/FindYoungestAndTallest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Arrays.Level_02
+{
+    internal class FindYoungestAndTallest
+    {
+        public void FindYoungest(int[] ages)
+        {
+            int youngest = ages[0];
+            for (int i = 1; i < ages.Length; i++)
+            {
+                if (ages[i] < youngest)
+                {
+                    youngest = ages[i];
+                }
+            }
+            Console.WriteLine("Youngest age is: " + youngest);
+        }
+        public void FindTallest(double[] heights)
+        {
+            double tallest = heights[0];
+            for (int i = 1; i < heights.Length; i++)
+            {
+                if (heights[i] > tallest)
+                {
+                    tallest = heights[i];
+                }
+            }
+            Console.WriteLine("Tallest height is: " + tallest);
+        }
+    }
+}
