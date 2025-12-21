3c560c28ec54c197f008ed5ed47fd79266ba9f3b
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-21 15:16:11 +0530

    [Prashant] add program to remove duplicate characters from string

---
+++ String/Level_02/RemoveDuplicate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.String.Level_02
+{
+    internal class RemoveDuplicate
+    {
+        /*
+         Remove Duplicates from a String
+        Problem: Write a C# program to remove all duplicate characters from a given string and return the
+        modified string
+        */
+
+        public void RemoveDuplicates(string input)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (!result.ToString().Contains(c))
+                {
+                    result.Append(c);
+                }
+            }
+            Console.WriteLine("String after removing duplicates: " + result.ToString());
+        }
+    }
+}
