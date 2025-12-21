e7f7350d3b6d6eed580ef3c85d458646c98d3bab
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-21 15:16:56 +0530

    [Prashant] add program to reverse the string

---
+++ String/Level_02/ReverseString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.String.Level_02
+{
+    internal class ReverseString
+    {
+        /*
+        Reverse a String
+        Problem: Write a C# program to reverse a given string without using any built-in reverse functions.
+        */
+        public void Reverse(string input)
+        {
+            char[] chars = input.ToCharArray();
+            int i = 0;
+            int j = input.Length-1;
+            while(i<j)
+            {
+                char temp = chars[i];
+                chars[i] = chars[j];
+                chars[j] = temp;
+                i++;
+                j--;
+            }
+            Console.WriteLine("Reverse String: " + string.Join("",chars));
+        }
+    }
+}
