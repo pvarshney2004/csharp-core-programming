fa1d1bc8e5d525a7540bbaf18c19e5e73846a8ea
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-21 13:57:23 +0530

    [Prashant] add program to demonstrate format exception

---
+++ String/Level_01/DemoFormatException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.String.Level_01
+{
+    internal class DemoFormatException
+    {
+        /*
+         Demonstrate FormatException
+        ● Hint =>
+            o Use int.Parse() on a non-numeric string to generate FormatException.
+            o Use try-catch to handle the exception.
+        */
+        public void ShowFormatException()
+        {
+            try
+            {
+                string s = "ab12";
+                int number = int.Parse(s);
+                Console.WriteLine(number);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("FormatException caught: " + e.Message);
+            }
+        }
+    }
+}
