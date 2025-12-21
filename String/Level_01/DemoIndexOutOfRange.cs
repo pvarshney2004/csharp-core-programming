ceb47b8a434beb3ecf1242f179ebd3f1b7eb0d37
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-21 13:57:57 +0530

    [Prashant] add program to demonstrate index out of range exception in array

---
+++ String/Level_01/DemoIndexOutOfRange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.String.Level_01
+{
+    internal class DemoIndexOutOfRange
+    {
+        /*
+         Demonstrate IndexOutOfRangeException for Arrays
+        ● Hint =>
+            o Access an invalid index of an array to generate an IndexOutOfRangeException.
+            o Use try-catch to handle the exception.
+        */
+        public void ShowIndexOutOfRangeException()
+        {
+            try
+            {
+                int[] numbers = { 1, 2, 3, 4, 5 };
+                // Accessing an invalid index
+                Console.WriteLine(numbers[10]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("IndexOutOfRangeException caught: " + e.Message);
+            }
+        }
+    }
+}
