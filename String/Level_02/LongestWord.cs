6679e29bc1cfd0ebc986faab3fba3aea8605718b
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-21 15:15:11 +0530

    [Prashant] add program to find longest word in sentence

---
+++ String/Level_02/LongestWord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.String.Level_02
+{
+    internal class LongestWord
+    {
+        /*
+        Find the Longest Word in a Sentence
+        Problem: Write a C# program that takes a sentence as input and returns the longest word in the
+        sentence.
+        */
+
+        public void FindLongestWord(string input)
+        {
+            input = input.ToLower();
+            string[] words = input.Split(' ');
+            string longestWord = "";
+            foreach (string word in words)
+            {
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+            }
+            Console.WriteLine("The longest word is: " + longestWord);
+        }
+    }
+}
