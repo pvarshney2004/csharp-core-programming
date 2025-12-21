9aef472b15b0a4f01f5987b289828ec5870fbfad
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-21 15:16:44 +0530

    [Prashant] add program to count substring occurence in string

---
+++ String/Level_02/SubstringOccurence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.String.Level_02
+{
+    internal class SubstringOccurence
+    {
+        /*
+        Find Substring Occurrences
+        Problem: Write a C# program to count how many times a given substring occurs in a string
+        */
+
+        public void Count(string str, string subStr)
+        {
+            int count = 0;
+            int index = 0;
+            while ((index = str.IndexOf(subStr, index, StringComparison.OrdinalIgnoreCase)) != -1)
+            {
+                count++;
+                index += subStr.Length;
+            }
+            Console.WriteLine($"The substring '{subStr}' occurs {count} times in the string.");
+        }
+    }
+}
