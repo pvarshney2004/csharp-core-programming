6c4a9be1d8ebb838554f450ee72bf172c8ebffed
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-21 15:16:24 +0530

    [Prashant] add program to remove specific characters from string

---
+++ String/Level_02/RemoveSpecificChar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.String.Level_02
+{
+    internal class RemoveSpecificChar
+    {
+        //Problem: Write a C# program to remove all occurrences of a specific character from a string.
+        public void RemoveChar(string input, char ch)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (c != ch)
+                {
+                    result.Append(c);
+                }
+            }
+            Console.WriteLine("Modified String: "+result.ToString());
+        }
+    }
+}
