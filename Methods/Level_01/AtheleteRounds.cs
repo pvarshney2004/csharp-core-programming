680b08f9075c2c3bf3b974f35d1d022f26de9989
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-22 15:57:48 +0530

    [Prashant] add program to find athelete rounds using methods

---
+++ Methods/Level_01/AtheleteRounds.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Methods.Level_01
+{
+    internal class AtheleteRounds
+    {
+        /*
+        An athlete runs in a triangular park with sides provided as input by the user in meters. If
+        the athlete wants to complete a 5 km run, then how many rounds must the athlete
+        complete.
+        */
+        public void CalculateAthleteRounds(double sideA, double sideB, double sideC)
+        {
+            double perimeter = sideA + sideB + sideC; // in meters
+            double totalDistance = 5000; // 5 km in meters
+            double rounds = totalDistance / perimeter;
+            Console.WriteLine($"The athlete must complete {Math.Ceiling(rounds)} rounds to complete a 5 km run.");
+        }
+    }
+}
