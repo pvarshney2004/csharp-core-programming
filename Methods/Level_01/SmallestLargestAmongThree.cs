67ae8ea92534154fa979f38ed33415bd03ff0819
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-22 16:01:25 +0530

    [Prashant] add program to find smallest and tallest using methods

---
+++ Methods/Level_01/SmallestLargestAmongThree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Methods.Level_01
+{
+    internal class SmallestLargestAmongThree
+    {
+        /*
+         * Write a program to find the smallest and the largest of the 3 numbers.
+        */
+        public int[] FindSmallestAndLargest(int num1, int num2, int num3)
+        {
+            int[] ans  = new int[2];
+            int smallest = Math.Min(num1, Math.Min(num2, num3));
+            int largest = Math.Max(num1, Math.Max(num2, num3));
+            ans[0] = smallest;
+            ans[1] = largest;
+            return ans;
+        }
+    }
+}
