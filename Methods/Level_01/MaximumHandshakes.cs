ed6b82b5c27c23f6467f99e010b0c876c8149513
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-22 16:00:30 +0530

    [Prashant] add program to find maximum handshakes using methods

---
+++ Methods/Level_01/MaximumHandshakes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Methods.Level_01
+{
+    internal class MaximumHandshakes
+    {
+        /*
+        Create a program to find the maximum number of handshakes among N number of students.
+        */
+        public void CalculateMaximumHandshakes(int numberOfStudents)
+        {
+            int maxHandshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+            Console.WriteLine($"The maximum number of handshakes among {numberOfStudents} students is: {maxHandshakes}");
+        }
+    }
+}
