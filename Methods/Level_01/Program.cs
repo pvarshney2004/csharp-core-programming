51f85648450155c83041787c2c8deb6707920a8e
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-22 16:02:47 +0530

    [Prashant] add Program.cs file

---
+++ Methods/Level_01/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Core_Programming.Methods.Level_01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //CalculateTrignometricFunc obj1 = new CalculateTrignometricFunc();
+            //double[] ans =  obj1.CalculateTrigonometricFunctions(60);
+            //Console.WriteLine("Trigonometric functions for 60 degrees:");
+            //Console.WriteLine($"Sine: {ans[0]}");
+            //Console.WriteLine($"Cosine: {ans[1]}");
+            //Console.WriteLine($"Tangent: {ans[2]}");
+
+            //CalculateWindChillTemp obj2 = new CalculateWindChillTemp();
+            //double windChill = obj2.ComputeWindChill(30, 10);
+            //Console.WriteLine(windChill);
+
+            //DivideNAmongM obj3 = new DivideNAmongM();
+            //obj3.DivideChocolates(10, 3);
+
+            //SmallestLargestAmongThree obj4 = new SmallestLargestAmongThree();
+            //int[] ans = obj4.FindSmallestAndLargest(23, 45, 33);
+            //Console.WriteLine($"Smallest among three: {ans[0]}");
+            //Console.WriteLine($"Largest among three: {ans[1]}");
+
+            //SumNaturals obj5 = new SumNaturals();
+            //obj5.CalculateSumOfNaturals(n: 8); // using key:value type to pass arguments
+
+            //PrintQoutientReminder obj6 = new PrintQoutientReminder();
+            //int[] ans = obj6.CalculateQuotientAndRemainder(23, 6);
+            //Console.WriteLine($"Qoutient: {ans[0]}, Reminder: {ans[1]}");
+
+            //SpringSeason obj7 = new SpringSeason();
+            //bool result = obj7.IsSpringSeason(month:12, day:22);
+            //if (result)
+            //{
+            //    Console.Write("It's spring season");
+            //}
+            //else
+            //{
+            //    Console.Write("It's not a spring season");
+            //}
+
+            //FindPosNegZero obj8 = new FindPosNegZero();
+            //int res = obj8.Find(23);
+            //if (res == 1) Console.Write("Positive");
+            //else if (res == -1) Console.Write("Negative");
+            //else Console.Write("Zero");
+
+            //SimpleInterest obj9 = new SimpleInterest();
+            //obj9.CalculateSimpleInterest(10000, 5, 3);
+
+            //MaximumHandshakes obj10 = new MaximumHandshakes();
+            //obj10.CalculateMaximumHandshakes(10);
+
+            //AtheleteRounds obj11 = new AtheleteRounds();
+            //obj11.CalculateAthleteRounds(3.0, 4.0, 5.0);
+
+
+        }
+    }
+}
