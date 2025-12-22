2d6dfb3844d9bfdf4b8f3705004390cbe0713921
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-22 16:01:42 +0530

    [Prashant] add program to find simple interest using methods

---
+++ Methods/Level_01/SimpleInterest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Methods.Level_01
+{
+    internal class SimpleInterest
+    {
+        /*
+        Write a program to input the Principal, Rate, and Time values and calculate Simple Interest.
+        */
+        public void CalculateSimpleInterest(double principal, double rate, double time)
+        {
+            double simpleInterest = (principal * rate * time) / 100;
+            Console.WriteLine($"The Simple Interest is: {simpleInterest}");
+        }
+    }
+}
