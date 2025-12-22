f7103503c6023f325d9b6ee2c30696930181a67d
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-22 16:00:54 +0530

    [Prashant] add program to print qoutient and reminder using methods

---
+++ Methods/Level_01/PrintQoutientReminder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Methods.Level_01
+{
+    internal class PrintQoutientReminder
+    {
+        /*
+         * Write a program to take 2 numbers and print their quotient and reminder
+        */
+        public int[] CalculateQuotientAndRemainder(int dividend, int divisor)
+        {
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+            return new int[] { quotient, remainder };
+        }
+
+    }
+}
