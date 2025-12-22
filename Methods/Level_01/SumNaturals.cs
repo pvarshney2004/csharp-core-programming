b84e614243394799e59437bdf334d310e4720c6d
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-22 16:02:20 +0530

    [Prashant] add program to find sum of natural numbers using methods

---
+++ Methods/Level_01/SumNaturals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Methods.Level_01
+{
+    internal class SumNaturals
+    {
+        /*
+        Write a program to find the sum of n natural numbers using loop
+        */
+        public void CalculateSumOfNaturals(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine($"The sum of first {n} natural numbers is: {sum}");
+        }
+    }
+}
