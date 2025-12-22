43529bd392965f37bfc8f396a1b287822959122c
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-22 16:00:11 +0530

    [Prashant] add program to find pos, neg and zero using methods

---
+++ Methods/Level_01/FindPosNegZero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Methods.Level_01
+{
+    internal class FindPosNegZero
+    {
+        /*
+        Write a program to check whether a number is positive, negative, or zero.
+        */
+        public int Find(int number)
+        {
+            if (number < 0)
+            {
+                return -1;
+            }
+            else if(number > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        
+    }
+}
