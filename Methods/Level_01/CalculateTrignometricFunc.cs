682f500871541097dde28479ccfabc0934574a21
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-22 15:58:47 +0530

    [Prashant] add program to find value of trignometric functions using methods

---
+++ Methods/Level_01/CalculateTrignometricFunc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Methods.Level_01
+{
+    internal class CalculateTrignometricFunc
+    {
+        /*
+         * Write a program to calculate various trigonometric functions using Math class given an angle in degrees
+        */
+        public double[] CalculateTrigonometricFunctions(double angleInDegrees)
+        {
+            double angleInRadians = angleInDegrees * (Math.PI / 180); // Convert degrees to radians
+            double sine = Math.Sin(angleInRadians);
+            double cosine = Math.Cos(angleInRadians);
+            double tangent = Math.Tan(angleInRadians);
+            return new double[] { sine, cosine, tangent};
+       
+        }
+    }
+}
