b0f1258a061881cde2effdfc549de70908d347dc
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-22 15:59:47 +0530

    [Prashant] add program to distribute n chochalate among m using methods

---
+++ Methods/Level_01/DivideNAmongM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Methods.Level_01
+{
+    internal class DivideNAmongM
+    {
+        /*
+         * Create a program to divide N number of chocolates among M children. Print the number
+           of chocolates each child will get and also the remaining chocolates
+        */
+        public void DivideChocolates(int numberOfChocolates, int numberOfChildren)
+        {
+            int chocolatesPerChild = numberOfChocolates / numberOfChildren;
+            int remainingChocolates = numberOfChocolates % numberOfChildren;
+            Console.WriteLine($"Each child will get {chocolatesPerChild} chocolates.");
+            Console.WriteLine($"Remaining chocolates: {remainingChocolates}");
+        }
+
+    }
+}
