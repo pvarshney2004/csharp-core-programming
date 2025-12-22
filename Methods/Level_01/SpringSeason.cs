0a7c54b6f880f29cfff800e1b7d57571b4d2d78f
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-22 16:02:03 +0530

    [Prashant] add program to find whether it is spring season or not using methods

---
+++ Methods/Level_01/SpringSeason.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Methods.Level_01
+{
+    internal class SpringSeason
+    {
+        /*
+        Write a program SpringSeason that takes two int values month and day from the
+        command line and prints “Its a Spring Season” otherwise prints “Not a Spring Season” 
+        */
+        public bool IsSpringSeason(int month, int day)
+        {
+            if ((month == 3 && day >= 20 && day <= 31) || (month == 4 && day >= 1 && day <= 30) || (month == 5 && day >= 1 && day <= 31) || (month == 6 && day >= 1 && day <= 20))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
