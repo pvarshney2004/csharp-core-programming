ac804dd29750ff410edf001dd70e12bda318bf7f
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-23 11:41:46 +0530

    [Prashant] add program to find check string is palindrome or not using built-in-functions

---
+++ Built_In_Functions/PalindromeString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Built_In_Functions
+{
+    internal class PalindromeString
+    {
+        /*
+Palindrome Checker:
+Write a program that checks if a given string is a palindrome (a word, phrase, or sequence
+that reads the same backward as forward).
+         */
+        public bool IsPalindrome(string str)
+        {
+            char[] chr = str.ToCharArray();
+            Array.Reverse(chr);
+            string reversedString = new string(chr);
+            return str == reversedString;
+
+        }
+    }
+}
