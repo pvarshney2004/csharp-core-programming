3b5b0a1aff661ab9609abbe9a3d7d07a5f45f52e
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-23 11:40:20 +0530

    [Prashant] add program to find factorial using recursion

---
+++ Built_In_Functions/FactorialUsingRecursion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Built_In_Functions
+{
+    internal class FactorialUsingRecursion
+    {
+        /*
+          Factorial Using Recursion: Write a program that calculates the factorial of a number using a recursive function
+         */
+        public int FindFactorial(int number)
+        {
+            if (number == 1) return 1;
+            return number * FindFactorial(number - 1);
+
+        }
+    }
+}
