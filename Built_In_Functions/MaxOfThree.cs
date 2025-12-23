024e26e73fd5609106cb62068e28cf862500b19e
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-23 11:42:10 +0530

    [Prashant] add program to find maximum of three numbers using built-in-functions

---
+++ Built_In_Functions/MaxOfThree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Built_In_Functions
+{
+    internal class MaxOfThree
+    {
+        /*
+Maximum of Three Numbers:
+Write a program that takes three integer inputs from the user and finds the maximum of the
+three numbers.
+         */
+        public int FindMaximum(int num1, int num2, int num3)
+        {
+            return Math.Max(num1, Math.Max(num2, num3));
+
+        }
+    }
+}
