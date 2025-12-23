4c6c678daa410343e4e7b76b1ec7f08ccca3b127
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-23 11:41:11 +0530

    [Prashant] add program to find GCD and LCM of two numbers

---
+++ Built_In_Functions/GCDAndLCM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Built_In_Functions
+{
+    internal class GCDAndLCM
+    {
+        /*
+GCD and LCM Calculator:
+Create a program that calculates the Greatest Common Divisor (GCD) and Least Common
+Multiple (LCM) of two numbers using functions.
+         */
+        public int FindGCD(int num1, int num2)
+        {
+            while(num1>0 && num2>0)
+            {
+                if (num1 > num2)
+                {
+                    num1 %= num2;
+                }
+                else
+                {
+                    num2 %= num1;
+                }
+            }
+            return (num1 == 0 ? num2 : num1);    
+        }
+        public int FindLCM(int num1, int num2)
+        {
+            return (num1*num2)/FindGCD(num1, num2);
+        }
+
+        public bool Verify(int num1, int num2)
+        {
+            return (num1*num2) == (FindGCD(num1, num2)*FindLCM(num1,num2));
+        }
+    }
+}
