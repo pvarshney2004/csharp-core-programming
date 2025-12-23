fbc0338b72b458ad3f8c9533f45042cee836b83c
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-23 15:10:12 +0530

    [Prashant] add program to print date in diffrent formats

---
+++ DateTimePractice/DateFormatting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.DateTimePractice
+{
+    internal class DateFormatting
+    {
+        /*
+        Problem 3: Date Formatting
+        Write a program that:
+        ● Displays the current date in three different formats:
+        o dd/MM/yyyy
+        o yyyy-MM-dd
+        o EEE, MMM dd, yyyy
+        Hint: Use DateTime.ToString() with custom date format strings.
+                 */
+        //method for formatting date
+        public void Format()
+        {
+            // getting current date from system
+            DateTime date = DateTime.Now;
+            // printing date in diffrent formats
+            Console.WriteLine(date.ToString("dd/MM/yyyy"));
+            Console.WriteLine(date.ToString("yyyy-MM-dd"));
+            Console.WriteLine(date.ToString("ddd, MMM dd, yyyy"));
+
+        }
+    }
+}
