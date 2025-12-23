08e63b920c5788df45800ba866262e7578832e47
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-23 11:43:42 +0530

    [Prashant] add program to convert Temperature units

---
+++ Built_In_Functions/TemperatureConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Built_In_Functions
+{
+    internal class TemperatureConverter
+    {
+        /*
+Temperature Converter:
+Write a program that converts temperatures between Fahrenheit and Celsius.
+● The program should have separate functions for converting from Fahrenheit to
+Celsius and from Celsius to Fahrenheit.
+         */
+        public double CelToFarh(double cel)
+        {
+            return (cel * 9 / 5) + 32;
+        }
+        public double FarhToCel(double farh)
+        {
+            return (farh - 32) * 5 / 9;
+        }
+    }
+}
