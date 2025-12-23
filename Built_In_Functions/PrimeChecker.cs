0bafe9cd709872d564bd6c7fd840c0de3d2bd8cb
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-23 11:43:17 +0530

    [Prashant] add program to check whether a number prime or not

---
+++ Built_In_Functions/PrimeChecker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Built_In_Functions
+{
+    internal class PrimeChecker
+    {
+        /*
+          Prime Number Checker: Create a program that checks whether a given number is a prime number.
+         */
+        public bool IsPrime(int number)
+        {
+            bool flag = true;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+        }
+    }
+}
