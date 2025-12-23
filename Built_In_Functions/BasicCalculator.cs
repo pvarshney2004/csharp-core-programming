14d8a636fedeab7133556c130ec5a945268a2dbe
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-23 11:39:54 +0530

    [Prashant] add program of Basic Calculations

---
+++ Built_In_Functions/BasicCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming.Built_In_Functions
+{
+    internal class BasicCalculator
+    {
+        /*
+Basic Calculator:
+Write a program that performs basic mathematical operations (addition, subtraction,
+multiplication, division) based on user input.
+● Each operation should be performed in its own function, and the program should
+prompt the user to choose which operation to perform.
+         */
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int Substraction(int a, int b)
+        {
+            return (a > b ? a - b : b - a);
+        }
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
